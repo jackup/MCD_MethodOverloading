6629ebccd3147c096402f2e19417b55b11432c5d
jackup <yk.58@hotmail.com>
2021-11-26 15:57:31 +0300

    Method Overloading

--- MCD_MethodOverloading/Program.cs
+++ MCD_MethodOverloading/Program.cs
@@ -10,6 +10,31 @@ namespace MCD_MethodOverloading
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Merhaba Dünya");
+            //Console.WriteLine(true);
+            decimal toplam = Topla(12M, 45M);
+            EkrandaGoster(toplam);
+
+            EkrandaGoster(Topla(12, 33));
+
+        }
+        static void EkrandaGoster(object o)
+        {
+            Console.WriteLine(o.ToString());
+        }
+        static int Topla(int sayi1, int sayi2)
+        {
+            int toplam = sayi1 + sayi2;
+            return toplam;
+        }
+        static decimal Topla(decimal sayi1, decimal sayi2)
+        {
+            decimal toplam = sayi1 + sayi2;
+            return toplam;
+        }
+        static string Topla(string metin1, string metin2)
+        {
+            return metin1 + " " + metin2;
         }
     }
 }
